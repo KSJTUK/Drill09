c2ee4fb033dcda1fe4ab8b81d5426c1d0242ed29
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-11 18:33:26 +0900

    IDLE에서 Sleep으로의 상태 변환 구현

--- boy.py
+++ boy.py
@@ -1,18 +1,21 @@
-from pico2d import load_image, SDL_KEYDOWN, SDLK_SPACE
+from pico2d import load_image, SDL_KEYDOWN, SDLK_SPACE, get_time
 import math
 
+
 def space_down(e):
-    return e[0] =='INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
 
 def time_out(e):
     return e[0] == 'TIME_OUT'
+
 
 # 원래 객체를 만드는 이유는 객체를 찍어내기 위해서이다
 # 파이썬에서는 객체 생성용이아닌 여러개의 함수를 모아 그룹으로 관리하는 기능이 있다.
 class IDLE:
     @staticmethod
     def enter(boy):
-        pass
+        boy.idle_start_time = get_time()
 
     @staticmethod
     def exit(boy):
@@ -21,11 +24,15 @@ class IDLE:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
+        if get_time() - boy.idle_start_time > 3:
+            boy.state_machine.handle_event(('TIME_OUT', 0))
 
     @staticmethod
     def draw(boy):
         boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100,
-                             boy.x, boy.y)
+                            boy.x, boy.y)
+
+
 class Sleep:
     @staticmethod
     def enter(boy):
@@ -42,7 +49,7 @@ class Sleep:
     @staticmethod
     def draw(boy):
         boy.image.clip_composite_draw(boy.frame * 100, boy.action * 100, 100, 100,
-                             math.pi / 2, ' ', boy.x - 25, boy.y - 25, 100, 100)
+                                      math.pi / 2, ' ', boy.x - 25, boy.y - 25, 100, 100)
 
 
 class StateMachine:
@@ -50,8 +57,8 @@ class StateMachine:
         self.boy = boy
         self.cur_state = Sleep
         self.transitions = {
-            Sleep: { space_down: IDLE },
-            IDLE: { time_out: Sleep}
+            Sleep: {space_down: IDLE},
+            IDLE: {time_out: Sleep}
         }
 
     def handle_event(self, e):
