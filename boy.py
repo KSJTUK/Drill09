badc9fe15d1c299a94da83c0397d6e53c25bada9
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-11 17:49:03 +0900

    StateMachine이 소년에 대한 정보를 받고 전달하도 변경

--- boy.py
+++ boy.py
@@ -4,34 +4,41 @@ from pico2d import load_image
 # 원래 객체를 만드는 이유는 객체를 찍어내기 위해서이다
 # 파이썬에서는 객체 생성용이아닌 여러개의 함수를 모아 그룹으로 관리하는 기능이 있다.
 class IDLE:
-
     @staticmethod
-    def enter():
+    def enter(boy):
         print("IDLE Entered")
         pass
 
     @staticmethod
-    def exit():
+    def exit(boy):
         print("IDLE Exit")
         pass
 
     @staticmethod
-    def do():
+    def do(boy):
         print("IDLE Do")
+        pass
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100,
+                             boy.x, boy.y)
         pass
 
 
 class StateMachine:
-    def __init__(self):
+    def __init__(self, boy):
+        self.boy = boy
         self.cur_state = IDLE
 
     def start(self):
-        self.cur_state.enter()
+        self.cur_state.enter(self.boy)
 
     def update(self):
-        self.cur_state.do()
+        self.cur_state.do(self.boy)
 
     def draw(self):
+        self.cur_state.draw(self.boy)
         pass
 
 
@@ -42,16 +49,14 @@ class Boy:
         self.dir = 0
         self.action = 3
         self.image = load_image('animation_sheet.png')
-        self.state_machine = StateMachine()
+        self.state_machine = StateMachine(self)
         self.state_machine.start()
 
     def update(self):
-        self.frame = (self.frame + 1) % 8
         self.state_machine.update()
 
     def handle_event(self, event):
         pass
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
         self.state_machine.draw()
