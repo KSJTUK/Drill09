a873d1558657afa7843a1e100174d9f4911d62f9
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-11 18:53:51 +0900

    Run상태 추가 및 Run상태 추가에 의한 상태 테이블 추가 구현

--- boy.py
+++ boy.py
@@ -1,5 +1,23 @@
-from pico2d import load_image, SDL_KEYDOWN, SDLK_SPACE, get_time
+from pico2d import load_image, SDL_KEYDOWN, SDL_KEYUP, \
+    SDLK_SPACE, get_time, \
+    SDLK_RIGHT, SDLK_LEFT
 import math
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
 def space_down(e):
@@ -14,11 +32,17 @@ def time_out(e):
 # 파이썬에서는 객체 생성용이아닌 여러개의 함수를 모아 그룹으로 관리하는 기능이 있다.
 class IDLE:
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
+        if boy.action == 0:
+            boy.action = 2
+        elif boy.action == 1:
+            boy.action = 3
+        boy.dir = 0
+        boy.frame = 0
         boy.idle_start_time = get_time()
 
     @staticmethod
-    def exit(boy):
+    def exit(boy, e):
         pass
 
     @staticmethod
@@ -33,13 +57,36 @@ class IDLE:
                             boy.x, boy.y)
 
 
-class Sleep:
+class Run:
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
+        if right_down(e) or left_up(e):
+            boy.dir, boy.action = 1, 1
+        elif left_down(e) or right_up(e):
+            boy.dir, boy.action = -1, 0
+
+    @staticmethod
+    def exit(boy, e):
         pass
 
     @staticmethod
-    def exit(boy):
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 5
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+
+class Sleep:
+    @staticmethod
+    def enter(boy, e):
+        boy.frame = 0
+        pass
+
+    @staticmethod
+    def exit(boy, e):
         pass
 
     @staticmethod
@@ -48,30 +95,34 @@ class Sleep:
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_composite_draw(boy.frame * 100, boy.action * 100, 100, 100,
-                                      math.pi / 2, ' ', boy.x - 25, boy.y - 25, 100, 100)
-
+        if boy.action == 2:
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100,
+                                        math.pi / 2, ' ', boy.x - 25, boy.y - 25, 100, 100)
+        else:
+            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100,
+                                        math.pi / 2, ' ', boy.x - 25, boy.y - 25, 100, 100)
 
 class StateMachine:
     def __init__(self, boy):
         self.boy = boy
-        self.cur_state = Sleep
+        self.cur_state = IDLE
         self.transitions = {
-            Sleep: {space_down: IDLE},
-            IDLE: {time_out: Sleep}
+            IDLE: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, time_out: Sleep},
+            Run: {right_down: IDLE, left_down: IDLE, right_up: IDLE, left_up: IDLE},
+            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: IDLE}
         }
 
     def handle_event(self, e):
         for check_event, next_state in self.transitions[self.cur_state].items():
             if check_event(e):
-                self.cur_state.exit(self.boy)
+                self.cur_state.exit(self.boy, e)
                 self.cur_state = next_state
-                self.cur_state.enter(self.boy)
+                self.cur_state.enter(self.boy, e)
                 return True
         return False
 
     def start(self):
-        self.cur_state.enter(self.boy)
+        self.cur_state.enter(self.boy, ('INPUT', 0))
 
     def update(self):
         self.cur_state.do(self.boy)
